6818f6299ba39796cf1eb49cef247d1dcaf3bfc6
Scoutzknifez <codyalexander@executivehomes.com>
2025-07-21 00:52:48 -0700

    add scores and indicator for active player

--- src/components/page-content/home-page-content/home-page-content.tsx
+++ src/components/page-content/home-page-content/home-page-content.tsx
@@ -5,9 +5,12 @@ import { sendPacket } from "../../../utils/packets/sendPacket";
 import {
     PacketType,
     TicTacToeCellData,
+    TicTacToeGameEndPacket,
+    TicTacToeGameEndReason,
     TicTacToeJoinRejectedPacket,
     TicTacToeJoinRequestPacket,
     TicTacToeJoinSuccessPacket,
+    TicTacToeScore,
     TicTacToeTurnPacket,
     TicTacToeUpdatePacket,
 } from "browser-game-shared";
@@ -30,7 +33,10 @@ export function HomePageContent({ gameServerUrl }: HomePageContent) {
 
     const [board, setBoard] = useState<TicTacToeCellData[]>(Array(9).fill(TicTacToeCellData.X));
     const [playerTurn, setPlayerTurn] = useState<PlayableMove>(TicTacToeCellData.X);
-    const [playerSymbol, setPlayerSymbol] = useState<PlayableMove>(TicTacToeCellData.X);
+    const [myPlayerSymbol, setMyPlayerSymbol] = useState<PlayableMove>(TicTacToeCellData.X);
+    const [gameScore, setGameScore] = useState<TicTacToeScore>({ o: 0, x: 0 });
+    const [gameEndReason, setGameEndReason] = useState<TicTacToeGameEndReason>();
+    const [gameWinner, setGameWinner] = useState<PlayableMove>();
 
     useEffect(() => {
         const webSocket = new WebSocket(gameServerUrl);
@@ -47,6 +53,20 @@ export function HomePageContent({ gameServerUrl }: HomePageContent) {
             const packet = parsePacket(event.data);
             console.log("Incoming packet: ", packet);
 
+            if (packet.type === PacketType.TIC_TAC_TOE_GAME_END) {
+                const ticTacToeGameEndPacket = packet as TicTacToeGameEndPacket;
+                const { score, reason, winner } = ticTacToeGameEndPacket;
+
+                setGameScore(score);
+                setGameEndReason(reason);
+
+                if (reason === TicTacToeGameEndReason.WINNER && winner) {
+                    setGameWinner(winner);
+                }
+
+                return;
+            }
+
             if (packet.type === PacketType.TIC_TAC_TOE_JOIN_REJECTED) {
                 const ticTacToeJoinRejectedPacket = packet as TicTacToeJoinRejectedPacket;
                 throw new Error(ticTacToeJoinRejectedPacket.reason);
@@ -54,20 +74,25 @@ export function HomePageContent({ gameServerUrl }: HomePageContent) {
 
             if (packet.type === PacketType.TIC_TAC_TOE_JOIN_SUCCESS) {
                 const ticTacToeJoinSuccessPacket = packet as TicTacToeJoinSuccessPacket;
-                const { board, turn, symbol } = ticTacToeJoinSuccessPacket;
+                const { score, board, turn, symbol } = ticTacToeJoinSuccessPacket;
 
+                setGameScore(score);
                 setBoard(board);
                 setPlayerTurn(turn as PlayableMove);
-                setPlayerSymbol(symbol as PlayableMove);
+                setMyPlayerSymbol(symbol as PlayableMove);
                 return;
             }
 
             if (packet.type === PacketType.TIC_TAC_TOE_UPDATE) {
                 const ticTacToeUpdatePacket = packet as TicTacToeUpdatePacket;
-                const { board, turn } = ticTacToeUpdatePacket;
+                const { score, board, turn } = ticTacToeUpdatePacket;
 
+                setGameScore(score);
                 setBoard(board);
                 setPlayerTurn(turn as PlayableMove);
+                setGameEndReason(undefined);
+                setGameWinner(undefined);
+                return;
             }
         });
 
@@ -85,13 +110,71 @@ export function HomePageContent({ gameServerUrl }: HomePageContent) {
         };
     }, [gameServerUrl]);
 
+    function getScore() {
+        if (myPlayerSymbol === TicTacToeCellData.O) {
+            return `${gameScore.o} - ${gameScore.x}`;
+        }
+
+        if (myPlayerSymbol === TicTacToeCellData.X) {
+            return `${gameScore.x} - ${gameScore.o}`;
+        }
+
+        return "0 - 0";
+    }
+
+    function getPlayerSymbolAndScoreTrackers() {
+        const isMyPlayerTurn = playerTurn === myPlayerSymbol;
+        const opponentSymbol = myPlayerSymbol === TicTacToeCellData.X ? TicTacToeCellData.O : TicTacToeCellData.X;
+        const isOpponentTurn = playerTurn === opponentSymbol;
+
+        const myPlayerClasses = classNames(
+            styles.playerSymbolTracker,
+            myPlayerSymbol === TicTacToeCellData.O ? styles.o : styles.x,
+            isMyPlayerTurn && styles.turn
+        );
+        const opponentClasses = classNames(
+            styles.playerSymbolTracker,
+            opponentSymbol === TicTacToeCellData.O ? styles.o : styles.x,
+            isOpponentTurn && styles.turn
+        );
+
+        return (
+            <div className={styles.playerSymbolAndScoreTrackersWrapper}>
+                <div className={myPlayerClasses}>
+                    <h3>You</h3>
+                    <span>{myPlayerSymbol}</span>
+                </div>
+                <h3 className={styles.scoreTracker}>{getScore()}</h3>
+                <div className={opponentClasses}>
+                    <h3>Opponent</h3>
+                    <span>{opponentSymbol}</span>
+                </div>
+            </div>
+        );
+    }
+
+    function getGameEndText() {
+        if (gameEndReason === TicTacToeGameEndReason.STALEMATE) {
+            return "stalemate";
+        }
+
+        if (gameEndReason === TicTacToeGameEndReason.WINNER && gameWinner) {
+            return `${gameWinner} won!`;
+        }
+    }
+
     function onCellClick(index: number) {
-        if (playerTurn !== playerSymbol) {
+        if (playerTurn !== myPlayerSymbol) {
             alert("Not your turn!");
             return;
         }
 
-        if (board[index] !== TicTacToeCellData.BLANK) {
+        if (
+            // If the cell is NOT empty
+            board[index] !== TicTacToeCellData.BLANK ||
+            // If the game is over for some reason
+            gameEndReason !== undefined
+        ) {
             return;
         }
 
@@ -107,24 +190,34 @@ export function HomePageContent({ gameServerUrl }: HomePageContent) {
         sendPacket(webSocket, ticTacToeTurnPacket);
     }
 
-    function getTicTacToeBoard() {
+    function getCell(cellData: TicTacToeCellData, index: number) {
+        const cellClasses = classNames(styles.cell, cellData === TicTacToeCellData.O ? styles.o : styles.x);
+
         return (
-            <div className={styles.ticTacToeBoard}>
-                {board.map((cellData, index) => (
-                    <div key={index} className={styles.cell} onClick={() => onCellClick(index)}>
-                        {cellData}
-                    </div>
-                ))}
+            <div key={index} className={cellClasses} onClick={() => onCellClick(index)}>
+                {cellData}
             </div>
         );
+    }
+
+    function getCells() {
+        return board.map(getCell);
+    }
+
+    function getTicTacToeBoard() {
+        return <div className={styles.ticTacToeBoard}>{getCells()}</div>;
     }
 
     const classes = classNames(styles.root);
 
     return (
         <div className={classes}>
-            <h1>Tic Tac Toe Board</h1>
-            {getTicTacToeBoard()}
+            <h1>Tic Tac Toe</h1>
+            {getPlayerSymbolAndScoreTrackers()}
+            <div className={styles.boardWrapper}>
+                <h2 className={styles.winnerText}>{getGameEndText()}</h2>
+                {getTicTacToeBoard()}
+            </div>
         </div>
     );
 }
